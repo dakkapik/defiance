c6ae0f3f7668df7c846f658b49ce0ff79c94ff54
BloodSport <ae86rodas@outlook.com>
2020-11-23 23:27:02 -0500

    working on react front end

--- client/src/pages/MissionControl.js
+++ client/src/pages/MissionControl.js
@@ -1,16 +1,60 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import '../style/MissionControl.css'
 
-const MissionControl = () => {
+const url = 'https://defiance.herokuapp.com/'
+
+export default function MissionControl (){
     return (
         <div className="body">
             <div className="map">
-                <div className="side-bar">side bar</div>
+                <SideBar/>
             </div>
             <div className="bottom-bar">bottom bar</div>
         </div>
     )
 }
 
+function SideBar () {
 
-export default MissionControl
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+
+        const items = [];
+
+        fetch(url+'api/users')
+        .then(res=>res.json())
+        .then(result=>{
+
+            for(let i = 0; i < result.length; i++){
+                items.push(
+                    <SideBarItem
+                        key={result[i]._id}
+                        firstName={result[i].firstName}
+                        employeeId={result[i].employeeId}
+                        isActive={result[i].isActive}
+                        isAdmin={result[i].isAdmin}
+                    />)
+            }
+
+            setUsers(items)
+        })
+        .catch(error=>{console.log('fetch error: ' + error)})
+
+    }, [])
+
+    return(
+        <div className="side-bar">
+            {users}
+        </div>
+    )
+}
+
+function SideBarItem (props) {
+    return (
+        <div className="side-bar-item">
+            {props.firstName}
+            {props.employeeId}
+        </div>
+    )
+}
