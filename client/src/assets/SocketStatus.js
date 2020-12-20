33e52c522f9b1d2d3989d22a683dfc2dc3ec6855
BloodSport <ae86rodas@outlook.com>
2020-12-20 09:55:08 -0500

    updated client websocket

--- client/src/assets/SocketStatus.js
+++ client/src/assets/SocketStatus.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import io from 'socket.io-client'
 
-const endpoint = "http://localhost:3001"
+const endpoint = "https://defiance.herokuapp.com/"
 
 export default function SocketStatus (props) {
 
