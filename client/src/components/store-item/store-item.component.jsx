17ff106013e1acc9dac45fa3c47e41b3fb9ed744
bryan aviles <easyrun32@gmail.com>
2021-01-13 13:19:22 -0500

    map updater

--- client/src/components/store-item/store-item.component.jsx
+++ client/src/components/store-item/store-item.component.jsx
@@ -4,14 +4,15 @@ import "./store-item.styles.scss";
 import { connect } from "react-redux";
 
 import { DriverSocketOn } from "../../redux/drivers/drivers.action";
-
-export const StoreItem = ({ storeInfo, DriverSocketOn }) => {
+import { setConnectedStore } from "../../redux/stores/stores.action";
+export const StoreItem = ({ storeInfo, DriverSocketOn, setConnectedStore }) => {
   return (
     <div className="storeitem">
       <div className="storeitem storeitem--text">{storeInfo.name}</div>
       <Button
         onClick={() => {
           DriverSocketOn(storeInfo);
+          setConnectedStore(storeInfo);
         }}
         variant="outlined"
         color="inherit"
@@ -24,6 +25,7 @@ export const StoreItem = ({ storeInfo, DriverSocketOn }) => {
 
 const mapDispatchToProps = (dispatch) => ({
   DriverSocketOn: (info) => dispatch(DriverSocketOn(info)),
+  setConnectedStore: (info) => dispatch(setConnectedStore(info)),
 });
 
 export default connect(null, mapDispatchToProps)(StoreItem);
--- client/src/redux/root-reducer.js
+++ client/src/redux/root-reducer.js
@@ -1,9 +1,10 @@
 import { combineReducers } from "redux";
 
 import driverReducer from "./drivers/drivers.reducer";
-
+import storesReducer from "./stores/stores.reducer";
 const rootReducer = combineReducers({
   drivers: driverReducer,
+  stores: storesReducer,
 });
 
 export default rootReducer;
