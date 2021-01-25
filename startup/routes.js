bf513e929a47266033b8d215006a438a29ee97c3
BloodSport <ae86rodas@outlook.com>
2021-01-25 14:12:08 -0500

    added places and directions route

--- startup/routes.js
+++ startup/routes.js
@@ -4,26 +4,30 @@ module.exports = function (app) {
   const express = require("express");
   const orders = require("../API/orders");
   const users = require("../API/users");
-  const react = require('../API/react')
-  const stores = require('../API/stores')
+  const react = require('../API/react');
+  const stores = require('../API/stores');
+  const directions = require("../API/directions")
+  const places = require("../API/places")
   const error = require("../middleware/error");
   
+  //this is main
   app.use(express.static(path.join(__dirname, '../client/build')));
   // app.use(express.static(path.join(__dirname, '../public')))
   
   app.use(express.json());
   
-  app.use(express.urlencoded({ extended: true }))
+  app.use(express.urlencoded({ extended: true }));
   
   app.use("/api/stores", stores);
   app.use("/api/react", react);
   app.use("/api/orders", orders);
   app.use("/api/users", users);
+  app.use("/api/directions", directions);
+  app.use("/api/places", places);
   app.use(error);
   
-  
-  app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../client/build/index.html'))
-  });
-  // react static view ^
+  //this is if invalid params send react static view
+  // app.get('*', (req, res)=>{
+  //   res.sendFile(path.join(__dirname, '../client/build/index.html'))
+  // });
 };
