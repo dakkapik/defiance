75e8a4f325bef97c14dc7b70b1245f6b067495b0
bryan aviles <easyrun32@gmail.com>
2021-02-10 01:33:06 -0500

    coverage report and css fixes along with renaming variables

--- client/src/components/map-sidebar/map-sidebar.component.jsx
+++ client/src/components/map-sidebar/map-sidebar.component.jsx
@@ -2,14 +2,19 @@ import React, { useState } from "react";
 
 import { connect } from "react-redux";
 import "./map-sidebar.styles.scss";
-import SocketStatus from "../socket-status/socket-status.component";
-
+import Button from "@material-ui/core/Button";
+import {
+  DriverSocketOff,
+  ClearActiveDriver,
+} from "../../redux/drivers/drivers.action";
+import { GenerateUser } from "../../components-test/generateuser.component";
+import SimUserMove from "../../components-test/move-user.component";
 import Map from "../map/map.component";
 import StoreList from "../store-list/store-list.component";
 import arrow from "./arrow.png";
 import Orders from "../orders/orders.component";
 
-const MapSideBar = ({ socket }) => {
+const MapSideBar = ({ socket, DriverSocketOff, ClearActiveDriver }) => {
   const [isexpanded, setisexpanded] = useState(false);
 
   return (
@@ -19,14 +24,32 @@ const MapSideBar = ({ socket }) => {
       {socket ? (
         <div className="sidebar-container">
           <div className={isexpanded ? "side-bar-expanded " : "side-bar"}>
-            <SocketStatus />
+            <div className="disconnect-container">
+              <GenerateUser id={3533} />
+              <GenerateUser id={4545} />
+
+              <Button
+                variant="outlined"
+                color="inherit"
+                onClick={() => {
+                  DriverSocketOff(false);
+                  ClearActiveDriver();
+                }}
+              >
+                Disconnect
+              </Button>
+              {/* 
+              <MoveUser id={4545} />
+                 */}
+              <SimUserMove id={5679} />
+            </div>
             <Orders isexpanded={isexpanded} />
           </div>
           <img
             src={arrow}
             alt="arrow"
             onClick={() => setisexpanded(!isexpanded)}
-            className={isexpanded ? "emoji-expanded" : "emoji"}
+            className={isexpanded ? "arrow-expanded" : "arrow"}
           />
         </div>
       ) : (
@@ -41,4 +64,10 @@ const MapSideBar = ({ socket }) => {
 const mapStateToProps = (state) => ({
   socket: state.drivers.socket,
 });
-export default connect(mapStateToProps, null)(MapSideBar);
+
+const mapDispatchToProps = (dispatch) => ({
+  DriverSocketOff: (bool) => dispatch(DriverSocketOff(bool)),
+  ClearActiveDriver: () => dispatch(ClearActiveDriver()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(MapSideBar);
