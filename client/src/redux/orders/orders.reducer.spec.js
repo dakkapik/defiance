e3d647aa25d5819f3d5a711abe26da434d99ffd8
bryan aviles <easyrun32@gmail.com>
2021-03-05 05:03:51 -0500

    creating a branch with test

---
+++ client/src/redux/orders/orders.reducer.spec.js
@@ -0,0 +1,8 @@
+import OrdersActionTypes from "./orders.types";
+import ordersReducer from "./orders.reducer";
+
+describe("Order Reducer", () => {
+  it("should return default state", () => {
+    console.log("hello worldsss");
+  });
+});
