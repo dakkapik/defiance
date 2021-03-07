3df37f4c09284eb79aa5de8268b7ab27b042b828
bryan aviles <easyrun32@gmail.com>
2021-03-07 06:15:33 -0500

    overall test

--- client/src/redux/orders/orders.reducer.spec.js
+++ client/src/redux/orders/orders.reducer.spec.js
@@ -1,8 +1,10 @@
 import OrdersActionTypes from "./orders.types";
 import ordersReducer from "./orders.reducer";
-
+import { expect } from "chai";
+//https://www.youtube.com/watch?v=UOGxfvNwv-8
 describe("Order Reducer", () => {
   it("should return default state", () => {
-    console.log("hello worldsss");
+    const newState = ordersReducer(undefined, {});
+    expect(newState).toEqual();
   });
 });
