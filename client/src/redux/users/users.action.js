f8a644670c84ce09d578a8f46edca591d5a11405
Andrija <andrijagajicbusiness@gmail.com>
2021-03-10 18:29:51 +0100

    Added template users

---
+++ client/src/redux/users/users.action.js
@@ -0,0 +1,16 @@
+import UsersActionTypes from './users.types'
+
+export const registerUser = (user) => ({
+    type: UsersActionTypes.REGISTER_USER,
+    payload: user
+})
+
+export const loginUser = (user) => ({
+    type: UsersActionTypes.LOG_IN_USER,
+    payload: user
+})
+
+export const disconnectUser = (user) => ({
+    type: UsersActionTypes.DISCONNECT_USER,
+    payload: user
+}) 
