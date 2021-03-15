6df09e357bfa63c21d6822c11cbb7f7673f38df1
Andrija <andrijagajicbusiness@gmail.com>
2021-03-15 23:31:11 +0100

    Made signin component

--- client/src/pages/sign-in/sign-in.component.jsx
+++ client/src/pages/sign-in/sign-in.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import './sing-in.styles.scss'
 
+import SignInForm from '../../components/sign-in-form/sign-in-form.component'
+
 const SignIn = () => {
     return (
         <div className='container'>
-
+            <SignInForm />
         </div>
     )
 };
