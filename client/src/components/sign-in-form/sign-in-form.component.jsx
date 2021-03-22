6c1f39999233d57f15d4116355911ef50b2f2dce
BloodSport <ae86rodas@outlook.com>
2021-03-22 12:17:45 -0400

    Revert "User can signin"
    This reverts commit 07bc4bf1088d6f6d695d6ceb23d12cd65630c472.

--- client/src/components/sign-in-form/sign-in-form.component.jsx
+++ client/src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import axios from 'axios'
 
 import "./sign-in-form.styles.scss";
 
@@ -13,14 +12,7 @@ function SignInForm() {
             alert('You must enter your user id!')
             return
         }
-        axios.get(`/api/users/${userid}`)
-            .then(function (response) {
-                console.log(response)
-            })
-            .catch(function (error) {
-                console.log(error)
-                alert('Sorry, there is no user with this id, try again')
-            })
+        console.log(userid)
     }
     console.log(userid);
   };
