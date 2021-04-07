ed5fc80cc0b5779a578ff2ad535c8a04051942fe
BloodSport <ae86rodas@outlook.com>
2021-04-07 10:11:48 -0400

    Update authentication.js

--- API/authentication.js
+++ API/authentication.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const { User, validateUser } = require("../models/user")
 const jwt = require('jsonwebtoken')
 
+//endpoint GET /:employeeId use this as the jwt with the db password and send it back to the device
 
 
 
