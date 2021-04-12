4bec2356cd3fab0b29f015e620e697527b30a75b
BloodSport <ae86rodas@outlook.com>
2021-04-12 19:48:28 -0400

    added geocode to the waypoints

--- socket/endpoints/orderBundles.js
+++ socket/endpoints/orderBundles.js
@@ -2,7 +2,7 @@ const { Order, validateOrderStatus } = require("../../models/order");
 const { OrderBundle, validateOrderBundle } = require("../../models/orderBundle")
 const getDirections = require("../../functions/getDirections");
 const { writeFileSync } = require("fs")
-// const mockDirections = require("../../../responce.json")
+const mockDirections = require("../../../directions/responce.json")
 
 const CURRENT_STORE = { lat: 26.286659103811463, lng: -80.19981933329022 }
 
@@ -17,16 +17,22 @@ module.exports = function (socket, orderBundleList, users) {
             waypoints.push(order.geocode)
         })
 
-        // console.log("sending directions to: ", user)
+        // console.log("sending directions to: ", orderBundle.id, user)
         // socket.to(user).emit("route", mockDirections)
         getDirections(origin, destination, waypoints)
         .then(res=>{
+            orderBundle.orders.forEach(( order, index ) => {
+                res.geocoded_waypoints[index + 1].coords = order.geocode
+            })
+            res.geocoded_waypoints[0].coords = CURRENT_STORE
+            res.geocoded_waypoints[res.geocoded_waypoints.length - 1].coords = CURRENT_STORE
+            
             console.log("sending directions to: ", orderBundle.id, user)
 
             updateOrders(orderBundle.orderIds)
             saveDirectionData(res)
             saveOrderBundle(orderBundle)
-            
+
             socket.to(user).emit("route", res)
         })
         .catch(err => console.log("getDirections ERROR: ", err))
@@ -41,8 +47,8 @@ module.exports = function (socket, orderBundleList, users) {
         const { error } = validateOrderStatus(status)
         if(error) return console.log("status validation error: ", error)
 
-        orderNumberArray.forEach(id => {    
-            Order.updateOne({orderNumber: id}, {status})
+        orderNumberArray.forEach(_id => {    
+            Order.updateOne({_id}, {status})
         })
     }
 
