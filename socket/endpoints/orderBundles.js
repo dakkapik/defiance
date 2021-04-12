1c70c5087524cdb898c5e147760f0118cf5cae02
BloodSport <ae86rodas@outlook.com>
2021-04-12 19:49:49 -0400

    _id update

--- socket/endpoints/orderBundles.js
+++ socket/endpoints/orderBundles.js
@@ -9,6 +9,7 @@ const CURRENT_STORE = { lat: 26.286659103811463, lng: -80.19981933329022 }
 module.exports = function (socket, orderBundleList, users) {
     
     orderBundleList.forEach(orderBundle => {
+        console.log(orderBundle)
         const user = findUserSocket(orderBundle.id)
         const origin = CURRENT_STORE;
         const destination = CURRENT_STORE;
@@ -36,13 +37,13 @@ module.exports = function (socket, orderBundleList, users) {
         return Object.keys(users).find( socketId =>{return users[socketId] == userId})
     }
 
-    function updateOrders(orderNumberArray){
+    function updateOrders(_ids){
         const status = "on_route"
         const { error } = validateOrderStatus(status)
         if(error) return console.log("status validation error: ", error)
 
-        orderNumberArray.forEach(id => {    
-            Order.updateOne({orderNumber: id}, {status})
+        _ids.forEach(_id => {    
+            Order.updateOne({_id}, {status})
         })
     }
 
